b5e55e9e63622105f83e0c3100307e889fee2643
Peter Gorniak <sumpfork@mailmight.com>
2020-10-31 13:57:44 -0700

    check-in before switching library

--- cdk/assets/lambda/lambda-handlers.py
+++ cdk/assets/lambda/lambda-handlers.py
@@ -3,9 +3,11 @@ import logging
 import os
 
 from flask_lambda import FlaskLambda
-from flask import request, send_file
+from flask import request, send_file, make_response
 from flask import render_template
 from flask_wtf import FlaskForm
+
+# from flask_wtf.csrf import CSRFProtect
 import wtforms.fields as wtf_fields
 import domdiv.main
 
@@ -16,6 +18,7 @@ PAGES = [
 ]
 
 flask_app = FlaskLambda(__name__)
+# CSRFProtect(flask_app)
 secret_key = os.environ["FLASK_SECRET_KEY"]
 assert secret_key, "Need secret key specified in env"
 flask_app.config["SECRET_KEY"] = secret_key
@@ -44,6 +47,7 @@ class DomDivForm(FlaskForm):
     expansions = wtf_fields.SelectMultipleField(
         choices=list(zip(choices, choiceNames)),
         label="Expansions to Include (Cmd/Ctrl click to select multiple)",
+        default=[choices[0]],
     )
 
     orientation = wtf_fields.SelectField(
@@ -53,32 +57,48 @@ class DomDivForm(FlaskForm):
 
 @flask_app.route("/", methods=["GET", "POST"])
 def root():
+    logger.info(f"root call, request is {request}, form is {request.form}")
+    # logger.info(f"session: {session} {session.get('csrf_token')}")
     logger.info(request)
-    form = DomDivForm()
+    form = DomDivForm(request.form)
+    logger.info(f"{form} - validate: {form.validate_on_submit()}")
+    logger.info(f"submitted: {form.is_submitted()}")
+    logger.info(f"validates: {form.validate()}")
+    logger.info(f"errors: {form.errors}")
+
     if form.validate_on_submit():
         options = domdiv.main.parse_opts([])
         logger.info(f"options before populate: {options}")
+        logger.info(f"expansions data: {form['expansions'].data}")
         form.populate_obj(options)
+        options.expansions = [[e] for e in form["expansions"].data]
+        logger.info(f"expansions data: {form['expansions'].data}")
         logger.info(f"options after populate: {options}")
         options = domdiv.main.clean_opts(options)
         logger.info(f"options after cleaning: {options}")
         buf = BytesIO()
         options.outfile = buf
         domdiv.main.generate(options)
-        buf.seek(0)
-        return send_file(
-            buf,
-            mimetype="application/pdf",
-            as_attachment=True,
-            attachment_filename="sumpfork_dominion_dividers.pdf",
-        )
-    return render_template(
+        logger.info("done generation, returning pdf")
+        r = make_response(buf.getvalue())
+        r.headers['Content-Type'] = 'application/pdf'
+        r.headers['Content-Disposition'] = 'attachment; filename="sumpfork_dominion_dividers.pdf"'
+        logger.info(f"response: {r}")
+        return r
+        # return send_file(
+        #     buf,
+        #     mimetype="application/pdf",
+        #     as_attachment=True,
+        #     attachment_filename="sumpfork_dominion_dividers.pdf".encode("utf-8"),
+        # )
+    r = render_template(
         "index.html",
         pages=PAGES,
         form=form,
         active="domdiv",
         static_url=os.environ["STATIC_WEB_URL"],
     )
+    return r
 
 
 @flask_app.route("/bar/", methods=["GET"])
