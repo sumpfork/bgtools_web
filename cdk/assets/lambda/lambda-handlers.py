09b02c9ea31b5a6ce09af8eee22250eed0b15431
Travis CI <travis@travis-ci.org>
2020-11-10 13:10:21 -0800

    checkpoint

--- cdk/assets/lambda/lambda-handlers.py
+++ cdk/assets/lambda/lambda-handlers.py
@@ -3,13 +3,13 @@ import logging
 import os
 
 import apig_wsgi
+import domdiv
 from flask import Flask, request, send_file, make_response
 from flask import render_template
 
 # from flask_wtf.csrf import CSRFProtect
 from domdiv_form import DomDivForm
-import tuckboxes
-import chitboxes
+from tuckbox_form import TuckboxForm
 
 PAGES = [
     ("domdiv", "Dominion Dividers"),
@@ -32,6 +32,7 @@ logger.setLevel(int(os.environ.get("LOG_LEVEL", logging.INFO)))
 #     response = apig_wsgi_handler_helper(event, context)
 #     print(json.dumps(response, indent=2, sort_keys=True))
 #     return response
+
 
 @flask_app.route("/", methods=["GET", "POST"])
 def root():
@@ -61,14 +62,34 @@ def root():
         form=form,
         active="domdiv",
         static_url=os.environ["STATIC_WEB_URL"],
+        version=domdiv.__version__,
+        version_url=f"https://github.com/sumpfork/dominiontabs/releases/tag/v{domdiv.__version__}",
     )
     return r
 
 
 @flask_app.route("/tuckboxes/", methods=["GET", "POST"])
 def tuckboxes():
+    form = TuckboxForm(request.form)
     logger.info("in tuckboxes!")
-    return {"statusCode": 200, "body": "tuckboxes"}
+    if form.validate_on_submit():
+        buf = form.generate()
+        r = send_file(
+            buf,
+            mimetype="application/pdf",
+            as_attachment=True,
+            attachment_filename="sumpfork_chitbox.pdf",
+        )
+        logger.info(f"response: {r}")
+        return r
+    r = render_template(
+        "index.html",
+        pages=PAGES,
+        form=form,
+        active="tuckboxes",
+        static_url=os.environ["STATIC_WEB_URL"]
+    )
+    return r
 
 @flask_app.route("/chitboxes/", methods=["GET", "POST"])
 def chitboxes():
--- cdk/assets/lambda/tuckbox_form.py
+++ cdk/assets/lambda/tuckbox_form.py
@@ -3,212 +3,71 @@ import logging
 import os
 
 import wtforms.fields as wtf_fields
+from wtforms import validators
 from flask_wtf import FlaskForm
-import tuckboxes
+from flask_wtf.file import FileField as FlaskFileField, FileAllowed
+from wtforms.validators import NumberRange
+from flask_uploads import UploadSet, IMAGES
+from tuckboxes.tuckboxes import TuckboxGenerator
 
 logger = logging.getLogger("tuckboxes")
 logger.setLevel(int(os.environ.get("LOG_LEVEL", logging.INFO)))
 
-class DomDivForm(FlaskForm):
-    # Expansions
-    choices = domdiv.main.EXPANSION_CHOICES
-    # make pretty names for the expansion choices
-    choiceNames = []
-    replacements = {
-        "1stedition": "1st Edition",
-        "2ndeditionupgrade": "2nd Edition Upgrade",
-        "2ndedition": "2nd Edition",
-    }
-    for choice in choices:
-        for s, r in replacements.items():
-            if choice.lower().endswith(s):
-                choiceNames.append("{} {}".format(choice[: -len(s)].capitalize(), r))
-                break
-        else:
-            choiceNames.append(choice.capitalize())
-    expansions = wtf_fields.SelectMultipleField(
-        label="Expansions to Include (Cmd/Ctrl click to select multiple)",
-        choices=list(zip(choices, choiceNames)),
-        default=["2ndedition"],
-    )
-    # Now Fan expansions
-    choices = domdiv.main.FAN_CHOICES
-    # make pretty names for the expansion choices
-    choiceNames = []
-    for choice in choices:
-        for s, r in replacements.items():
-            if choice.lower().endswith(s):
-                choiceNames.append('{} {}'.format(
-                    choice[:-len(s)].capitalize(), r))
-                break
-        else:
-            choiceNames.append(choice.capitalize())
-    fan_expansions = wtf_fields.SelectMultipleField(
-        choices=list(zip(choices, choiceNames)),
-        label='Fan Expansions to Include (Cmd/Ctrl click to select multiple)',
-    )
-    orientation = wtf_fields.SelectField(
-        label="Divider Orientation",
-        choices=["Horizontal", "Vertical"],
-        default="Horizontal",
-    )
+images = UploadSet("images", IMAGES)
 
-    pagesize = wtf_fields.SelectField(
-        label="Paper Size",
-        choices=list(
-            zip(
-                PAPER_SIZES + domdiv.main.LABEL_KEYS,
-                PAPER_SIZES + domdiv.main.LABEL_SELECTIONS,
-            )
-        ),
-        default="Letter",
-    )
 
-    choices = ["Sleeved - Thin", "Sleeved - Thick", "Unsleeved"]
-    cardsize = wtf_fields.SelectField(
-        choices=list(zip(choices, choices)), label="Card Size", default="Unsleeved"
+class TuckboxForm(FlaskForm):
+    width = wtf_fields.DecimalField(
+        label="Width in cm (1-20)",
+        validators=[validators.NumberRange(1.0, 20.0)],
+        default=6,
     )
-    tabwidth = wtf_fields.DecimalField(
-        label='Width of Tab in centimeters',
-        default=4.0,
+    height = wtf_fields.DecimalField(
+        label="Height in cm (1-20)",
+        validators=validators.NumberRange(1.0, 20.0),
+        default=9.3,
     )
-    back_offset = wtf_fields.DecimalField(
-        label='Back page horizontal offset points to shift to the right',
-        default=0,
-        )
-    back_offset_height = wtf_fields.DecimalField(
-        label='Back page vertical offset points to shift upward',
-        default=0,
-        )
-
-    horizontal_gap = wtf_fields.DecimalField(
-        label='Horizontal gap between dividers in centimeters',
-        default=0,
-        )
-    vertical_gap = wtf_fields.DecimalField(
-        label='Vertical gap between dividers in centimeters',
-        default=0,
-        )
-
-    black_tabs = wtf_fields.BooleanField(
-        label='Black tab background',
+    depth = wtf_fields.DecimalField(
+        label="Depth in cm (1-20)",
+        validators=validators.NumberRange(1.0, 20.0),
+        initial=3,
     )
-    base_cards_with_expansion = wtf_fields.BooleanField(
-        label="Include Base cards with the expansion", default=False)
-    upgrade_with_expansion = wtf_fields.BooleanField(
-        label="Include upgrade cards with the expansion being upgraded",
-        default=False)
-    edition = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.EDITION_CHOICES, domdiv.main.EDITION_CHOICES)),
-        label='Edition',
-        default='latest')
-    cropmarks = wtf_fields.BooleanField(
-        label="Cropmarks Instead of Outlines", default=False)
-    linetype = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.LINE_CHOICES,
-                domdiv.main.LINE_CHOICES)),
-        label='Outline Type',
-        default='line')
-    wrappers = wtf_fields.BooleanField(
-        label="Slipcases Instead of Dividers", default=False)
-    notch = wtf_fields.BooleanField(
-        label="If Slipcases, add a notch in corners", default=False)
-    serpentine = wtf_fields.BooleanField(
-        label="For 3 or more tabs, tab location reverses when the end is reached instead of resetting to the start",
-        default=False)
-    reset_tabs = wtf_fields.BooleanField(
-        label="Restart tab starting location with every expansion.",
-        default=True)
-    counts = wtf_fields.BooleanField(
-        label="Show number of Cards per Divider", default=False)
-    types = wtf_fields.BooleanField(
-        label="Show Card Type on each Divider", default=False)
-    tab_name_align = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.NAME_ALIGN_CHOICES,
-                domdiv.main.NAME_ALIGN_CHOICES)))
-    tab_number = wtf_fields.SelectField(
-        choices=list(TAB_NUMBER_SELECTION.items()),
-        label='Number of tabs',
-        default=1)
-
-    for x in domdiv.main.TAB_SIDE_CHOICES:
-        if x not in TAB_SIDE_SELECTION:
-            TAB_SIDE_SELECTION[x] = x.title()
-    tab_side = wtf_fields.SelectField(
-        choices=list(TAB_SIDE_SELECTION.items()),
-        label='Starting tab location',
-        default='left')
-    
-    samesidelabels = wtf_fields.BooleanField(
-        label="Same Side Labels", default=False)
-    order = wtf_fields.SelectField(
-        label="Divider Order",
-        choices=list(
-            zip(domdiv.main.ORDER_CHOICES, domdiv.main.ORDER_CHOICES)))
-    group_special = wtf_fields.BooleanField(
-        label="Group Special Cards (e.g. Prizes)", default=True)
-    expansion_dividers = wtf_fields.BooleanField(
-        label="Include Expansion Dividers", default=False)
-    centre_expansion_dividers = wtf_fields.BooleanField(
-        label="If Expansion Dividers, centre the tabs on expansion dividers",
-        default=False)
-    expansion_dividers_long_name = wtf_fields.BooleanField(
-        label=("If Expansion Dividers, use edition "
-               "on expansion dividers names"),
-        default=False)
-    set_icon = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.LOCATION_CHOICES, domdiv.main.LOCATION_CHOICES)),
-        label="Set Icon Location",
-        default="tab")
-    cost_icon = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.LOCATION_CHOICES, domdiv.main.LOCATION_CHOICES)),
-        label="Cost Icon Location",
-        default="tab")
-    language = wtf_fields.SelectField(
-        choices=list(
-            zip(domdiv.main.LANGUAGE_CHOICES, domdiv.main.LANGUAGE_CHOICES)),
-        label='Language',
-        default='en_us')
-    events = wtf_fields.BooleanField(
-        label="Exclude Individual Events & Landmarks", default=False)
-    divider_front_text = wtf_fields.SelectField(
-        label='Front Text',
-        choices=list(zip(domdiv.main.TEXT_CHOICES, domdiv.main.TEXT_CHOICES)),
-        default='card')
-    divider_back_text = wtf_fields.SelectField(
-        label='Back Text',
-        choices=list(
-            zip(domdiv.main.TEXT_CHOICES + ['none'],
-                domdiv.main.TEXT_CHOICES + ['no back page'])),
-        default='rules')
-    no_footer = wtf_fields.BooleanField(
-        label='Omit the expansion name at the bottom of the page', default=False)
-    tag = wtf_fields.HiddenField(default='domdiv')
+    front_image = FlaskFileField(
+        label="Upload Main Image", validators=[FileAllowed(images, "Images only!")]
+    )
+    side_image = FlaskFileField(
+        label="Upload Side Image", validators=[FileAllowed(images, "Images only!")]
+    )
+    back_image = FlaskFileField(
+        label="Upload Back Image", validators=[FileAllowed(images, "Images only!")]
+    )
+    end_image = FlaskFileField(
+        label="Upload End Image", validators=[FileAllowed(images, "Images only!")]
+    )
+    preserve_side_aspect = wtf_fields.BooleanField(
+        label="Preserve Side Image Aspect", default=True
+    )
+    preserve_end_aspect = wtf_fields.BooleanField(
+        label="Preserve End Image Aspect", default=True
+    )
+    fill_colour = wtf_fields.StringField(default="#99FF99")
+    #        widget=forms.TextInput(attrs={'type': 'color'}), initial='#99FF99')
+    tag = wtf_fields.HiddenField(default="tuckboxes")
 
     def generate(self):
-        options = domdiv.main.parse_opts([])
-        logger.info(f"options before populate: {options}")
-        self.populate_obj(options)
-        options.expansions = [[e] for e in self["expansions"].data]
-        options.fan_expansions = [[e] for e in self["fan_expansions"].data]
-        options.tab_number = int(options.tab_number)
-        options.tabwidth = float(options.tabwidth)
-        options.vertical_gap = float(options.vertical_gap)
-        options.horizontal_gap = float(options.horizontal_gap)
-        options.back_offset = float(options.back_offset)
-        options.back_offset_height = float(options.back_offset_height)
-
-        logger.info(f"options after populate: {options}")
-        options = domdiv.main.clean_opts(options)
-        logger.info(f"options after cleaning: {options}")
         buf = BytesIO()
-        options.outfile = buf
-        domdiv.main.generate(options)
-        logger.info("done generation, returning pdf")
+        fc = re.match(r"#(\w{2})(\w{2})(\w{2})", self["fill_colour"]).groups()
+        fc = tuple(int(p, 16) / 255.0 for p in fc)
+        c = TuckboxGenerator.fromRawData(
+            self["width"],
+            self["height"],
+            self["depth"],
+            buf,
+            fillColour=fc,
+            preserveSideAspect=self["preserve_side_aspect"],
+            preserveEndAspect=self["preserve_end_aspect"],
+        )
+        c.generate()
+        c.close()
         buf.seek(0)
         return buf
