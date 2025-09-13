90d4c313b77e87a199cc41444819e2f8860682ec
Peter Gorniak <domdiv@petergorniak.org>
2025-09-13 11:23:52 -0700

    comment

--- assets/lambda/domdiv_form.py
+++ assets/lambda/domdiv_form.py
@@ -43,6 +43,7 @@ class DomDivForm(FlaskForm):
         "andguilds2ndedition": "and Guilds 2nd Edition",
         "2ndedition": "2nd Edition",
         "-bigbox2-de": "(Deutsche Big Box v2)",
+        # next shouldn't be needed anymore, test and take out
         "risingsun": "Rising Sun",
     }
     for choice in expansion_choices:
